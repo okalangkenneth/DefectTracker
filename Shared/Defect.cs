ac87ad198be2748e345714f006cfd60f1ceca536
Kenneth Okalang <okalang.ok@gmail.com>
2021-07-27 16:29:26 +0200

    Created Defect and Defect status classe in shared project

---
+++ Shared/Defect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Defect_Tracker.Shared
+{
+    public class Defect
+    {
+        public int Id { get; set; }
+        [Required]
+        public string DefectStatus { get; set; }
+        [Required]
+        public DateTime DefectAt { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2,
+
+        ErrorMessage ="Description must be a minimum of 2 and maximum of 50 characters.")]
+        public string DefectDescription { get; set; }
+        [Required]
+        [EmailAddress]
+        public string DefectReporterEmail { get; set; }
+        public string DefectGuid { get; set; }
+        public List<DefectDetail> DefectDetails {get; set;}
+    }
+    public class DefectDetail
+    {
+        public int Id { get; set; }
+        public int DefectId { get; set; }
+        public DateTime DefectAt { get; set; }
+        public string DefectDescription { get; set; }
+    }
+}
+
---
+++ Shared/DefectStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Defect_Tracker.Shared
+{
+    public class DefectStatus
+    {
+        public string ID { get; set; }
+        public string Text { get; set; }
+
+        public static List<DefectStatus> Statuses = new List<DefectStatus>() {
+        new DefectStatus(){ ID= "New", Text= "New" },
+        new DefectStatus(){ ID= "Open", Text= "Open" },
+        new DefectStatus(){ ID= "Urgent", Text= "Urgent" },
+        new DefectStatus(){ ID= "Closed", Text= "Closed" },
+        };
+    }
+
+}
